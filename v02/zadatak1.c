69101149965781ea9d8ea744d9c0c7f5b1826c6f
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-30 02:13:19 +0200

    Update zadatak1.c

--- v02/zadatak1.c
+++ v02/zadatak1.c
@@ -1,45 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum operacija { sabiranje, oduzimanje, mnozenje, deljenje };
+enum kodOperacije { sabiranje, oduzimanje, mnozenje, deljenje };
 
-float* Calculate(const char* inBuff) {
+// float * Calculate(const char *inBuff);
+float * Calculate(const char *inBuff)
+{
+	int *operand = (int *)(inBuff + 1);
+	float *rez=NULL;
+	rez = (float *)malloc(sizeof(float));
 
-	char izbor = *inBuff;
-	int* a = (int*)(inBuff + 1 * sizeof(char));
-	int* b = (int*)(inBuff + 5 * sizeof(char));
-
-	float* rez = (float*)malloc(sizeof(float));
-
-	// *p == niz[0]
-	switch (izbor) {
-
-	case sabiranje:
-		*rez = (float)(*a + *b);
-		break;
-	case oduzimanje:
-		*rez = (float)(*a - *b);
-		break;
-	case mnozenje:
-		*rez = (float)(*a * *b);
-		break;
-	case deljenje:
-		*rez = (float)(*a / *b);		// samo ovo radi kako treba
-		break;
+	switch (inBuff[0])
+	{
+		case sabiranje:
+			*rez = (float)(operand[0] + operand[1]);
+			break;
+		case oduzimanje:
+			*rez = (float)(operand[0] - operand[1]);
+			break;
+		case mnozenje:
+			*rez = (float)(operand[0] * operand[1]);
+			break;
+		case deljenje:
+			*rez = (float)operand[0] / operand[1];
+			break;
 	}
 
-	return rez;
+	return  rez;
 }
 
-int main(int argc, char* argv[])
+
+int main(int argc, char *argv[])
 {
-	char niz[9] = { 1, 0, 0, 0, 12, 0, 0, 0, 3 };		// računa: 12 - 3
+	// zauzimamo bafer koji cemo proslediti funkciji
+	char *p_ParametarFunkcije = (char *)malloc(sizeof(char) + 2 * sizeof(int));
+	char operacija;
 
-	float* rezultat;
-	rezultat = Calculate(niz);
+	printf("Unesite operaciju koju zelite da koristite : ");
+	scanf("%c", &operacija);
+    fflush(stdin);
+	
+	// smestanje koda operacije u prvi bajt
+	if (operacija == '+')
+		p_ParametarFunkcije[0] = sabiranje; 
+	else if (operacija == '-')
+		p_ParametarFunkcije[0] = oduzimanje;
+	else if (operacija == '*')
+		p_ParametarFunkcije[0] = mnozenje;
+	else if (operacija == '/')
+		p_ParametarFunkcije[0] = deljenje;
+	else return 0;
 
+	int op1, op2;
+
+	// unos operanada
+	printf("Unesite prvi broj : ");
+	scanf("%d", &op1);
+	getchar();
+
+	printf("Unesite drugi broj : ");
+	scanf("%d", &op2);
+	getchar();
+
+	// pokazuje na 2. mesto u baferu 
+	int *operand = (int *)(p_ParametarFunkcije + sizeof(char));
+
+	// smestamo operande na njihovu poziciju u baferu
+	operand[0] = op1;
+	operand[1] = op2;
+
+	float *rezultat;
+
+	rezultat = Calculate(p_ParametarFunkcije);
 	printf("Resenje je : %f\n", *rezultat);
+
+	// obavezno oslobadjamo dinamicki alociranu memoriju
 	free(rezultat);
+	free(p_ParametarFunkcije);
 
 	return 0;
 }
+
