7e41af577a70e7bf50c995284c53169e0115f32e
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-30 02:17:46 +0200

    Create main.c

---
+++ v05/zadatak2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "BitsBuffer.h"
+
+int main()
+{
+    bitbuffer b;
+    initbuff(&b);
+
+    char message[] = "010010000110010101101100011011000110111100100000011101110110111101110010011011000110010000100001";
+	
+    for(int i = 0; i < strlen(message); i++) {
+        push(&b, message[i] - '0');
+    }
+
+    while(!isEmpty(b)) {
+        char c;
+
+        for (int i = 0; i < 8; i++){
+            int num = pop(&b);
+            setBit(&c, i, num);
+        }
+
+        printf("%c", c);
+    }
+
+    return 0;
+}
