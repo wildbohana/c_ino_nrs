62b5001199c10b36c6b2afd96b055916178046c1
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-30 02:15:03 +0200

    Create zadatak1.c

---
+++ v04/zadatak1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *konverzija(int n){
+	char s[33], *rez;
+	s[32] = 0;
+	int i = 31;
+
+	do {
+    	if (n % 2 == 0) {
+    		s[i] = '0';
+		}
+    	else {
+        	s[i] = '1';
+		}
+
+     	i--;
+     	n = n / 2;
+   	} while (n != 0);
+
+   	rez = (char *)malloc(32 - i);
+   	memcpy(rez, &s[i + 1], 32 - i);
+
+   	return rez;
+}
+
+int main()
+{
+    int n;
+    char *s;
+
+    printf("Unesite dekadni broj koji treba kovertovati: ");
+    scanf("%d", &n);
+	
+    s = konverzija(n);
+    printf("Binarni broj izgleda ovako: %s\n", s);
+	
+    return 0;
+}
