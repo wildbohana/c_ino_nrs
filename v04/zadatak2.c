d0cc938e88de4074c6b2bcf4c7fea77416fae5e0
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-03-30 02:15:14 +0200

    Create zadatak2.c

---
+++ v04/zadatak2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int konverzija(char *s){
+	int rez, i;
+   	rez = 0;
+   	i = 0;
+
+   	while (s[i] != 0) {
+    	rez = rez * 2 + s[i] - '0';
+    	i++;
+   	}
+   	return rez;
+}
+
+int main()
+{
+    int n;
+    char s[20];
+
+    printf("Unesite binarni broj koji treba kovertovati: ");
+    scanf("%s", &s);
+
+    n = konverzija(s);
+    printf("Dekadni broj izgleda ovako: %d\n", n);
+	
+    return 0;
+}
