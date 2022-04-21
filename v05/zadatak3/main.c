ccf3ebd0c141523414c0af30f895e5695a28158f
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-21 22:57:05 +0200

    Update main.c

--- v05/zadatak3/main.c
+++ v05/zadatak3/main.c
@@ -1,8 +1,18 @@
+/*
+Napisati program koji učitava broj X tipa int i konvertuje ga u binarni sistem u obliku stringa.
+
+Program treba da ima funkciju sa zaglavljem:
+char* konverzija(int x);
+
+Funkciju realizovati Koristeći programiranja niskog nivoa, 
+tako što će se bitovi rezultata čitati direktno iz broja X.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char *konverzija(int n){
+char* konverzija(int n){
 	char s[33], *rez;
 	int i, mask, num;
 
@@ -13,21 +23,22 @@ char *konverzija(int n){
 	for (i = 0; i < 32; i++) {
 		if ((n & mask) == 0) {
 			s[31-i] = '0';
-		} else {
+		} 
+		else {
 			s[31-i] = '1';
 			num = i;
 		}
 		mask = mask << 1;
 	}
 
-	rez = (char *) malloc(num + 2);
+	rez = (char*) malloc(num + 2);
 	memcpy(rez, &s[31 - num], num + 2);
 	return rez;
 }
 
 int main() {
 	int n;
-	char *s;
+	char* s;
 
 	printf("Unesite broj koji treba kovertovati: ");
 	scanf("%d", &n);
