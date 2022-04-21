cc563d1bf7b092831cad01a5c2ce522374215a98
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-21 22:57:22 +0200

    Update main.c

--- v05/zadatak4/main.c
+++ v05/zadatak4/main.c
@@ -1,7 +1,17 @@
+/*
+Napisati program koji učitava broj X u binarnom sistemu u obliku stringa i konvertuje taj broj u tip podataka int.
+
+Program treba da ima funkciju sa zaglavljem:
+int konverzija(char* s);
+
+Funkciju realizovati koristeći programiranje niskog nivoa, 
+tako što će se bitovi upisivati u rezultat korišćenjem programiranja niskog nivoa.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int konverzija(char *s) {
+int konverzija(char* s) {
 	int rez, mask, i;
 	rez = 0;
 
