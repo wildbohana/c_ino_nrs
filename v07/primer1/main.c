a45380bf67dd800b15d90b3dfbc38c4ca4a17f79
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-21 22:58:24 +0200

    Update main.c

--- v07/primer1/main.c
+++ v07/primer1/main.c
@@ -1,27 +1,38 @@
+/*
+Napisati program koji unosi broj N i matricu A dimenzije NxN i izračunava njenu determinantu koristeći razvoj matrice po nultoj vrsti.
+Program rešiti rekurzivno.
+
+Razvoj matrice po i-toj vrsti: ~ formula ~
+
+Matrica Bi,j predstavlja matricu koja se dobija od matrice A izbacivanjem i-te vrste i j-te kolone.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdlib.h>
 
-int izracunajDeterminantu(int *buffer){
+int izracunajDeterminantu(int* buffer){
     int n = buffer[0];
 
     if (n == 1) {
         return buffer[1];
-	} else if (n == 2) {
+	}
+    else if (n == 2) {
         return buffer[1] * buffer[4] - buffer[2] * buffer[3];
-    } else {
+    } 
+	else {
         int i, j, kol, suma = 0;
         int A[n][n];
 
         for (j = 0; j < n; j++)
             for (i = 0; i < n; i++)
-                A[j][i] = buffer[j * n + i + 1];	// +1 zbog N
+                A[j][i] = buffer[j * n + i + 1]; 		// +1 zbog N
 
         int buffer2[(n - 1) * (n - 1) + 1];
         buffer2[0] = n - 1;
 
         for (kol = 0; kol < n; kol++) {
-            // razvoj po 0-toj vrsti, vrsta j==0 se preskace
+            // razvoj po 0-toj vrsti, vrsta j == 0 se preskace
             // preskace se i kolona kol
             for (j = 1; j < n; j++)
                 for (i = 0; i < n; i++)
@@ -29,6 +40,7 @@ int izracunajDeterminantu(int *buffer){
                         buffer2[(j - 1) * (n - 1) + i + 1] = A[j][i];
                     else if (i > kol)
                         buffer2[(j - 1) * (n - 1) + (i - 1) + 1] = A[j][i];
+
             suma += pow(-1, kol) * A[0][kol] * izracunajDeterminantu(buffer2);
         }
         return suma;
@@ -37,26 +49,27 @@ int izracunajDeterminantu(int *buffer){
 
 int main()
 {
-	int i, j, n;
+    int i, j, n;
 
-	printf("Unesite dimenzije matrice: ");
-	scanf("%d", &n);
-	int A[n][n];
+    printf("Unesite dimenzije matrice: ");
+    scanf("%d", &n);
 
-	printf("Unesite elemente matrice: ");
-	for (j = 0; j < n; j++)
-		for (i = 0; i < n; i++)
-			scanf("%d", &A[j][i]);
+    int A[n][n];
 
-	int buffer[n * n + 1];
-	buffer[0] = n;
+    printf("Unesite elemente matrice: ");
+    for (j = 0; j < n; j++)
+        for (i = 0; i < n; i++)
+            scanf("%d", &A[j][i]);
 
-	for (j = 0; j < n; j++)
-		for (i = 0; i < n; i++)
-			buffer[j * n + i + 1] = A[j][i]; 	// +1 je zbog broja N
+    int buffer[n * n + 1];
+    buffer[0] = n;
 
-	int rez = izracunajDeterminantu(buffer);
-	printf("Determinanta matrice je: %d", rez);
+    for (j = 0; j < n; j++)
+        for (i = 0; i < n; i++)
+            buffer[j * n + i + 1] = A[j][i]; 	// +1 je zbog broja N
+
+    int rez = izracunajDeterminantu(buffer);
+    printf("Determinanta matrice je: %d", rez);
 	
-	return 0;
+    return 0;
 }
