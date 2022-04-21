adbb2939c9ac849b6c3ca92824cd99b20a8d556c
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-21 22:58:46 +0200

    Update main.c

--- v07/zadatak1/main.c
+++ v07/zadatak1/main.c
@@ -1,26 +1,34 @@
+/*
+Napraviti listu koja sadrži podatke o N studenata i smestiti kreiranu listu u bafer.
+Za svakog studenta zapamtiti ime, prezime, broj indeksa i broj položenih ispita.
+Napisati funkciju izdvoj() koja iz datog bafera izdvaja sve studente koji su položili više od 5 ispita.
+
+Zaglavlje funkcije dato je na sledeći način:
+list izdvoji(char* buffer);
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
 
 int main()
 {
-    node *l;
+    node* l;
     student st;
     int n, i;
-
     init(&l);
 
     // ucitavanje studenata
     printf("Unesite broj studenata: ");
     scanf("%d", &n);
     for (i = 0; i < n; i++) {
-        printf("Unesite podatke o studentu broj %d: \n", i + 1);
+        printf("Unesite podatke o studentu broj %d: \n", i+1);
         scanf("%s%s%s%d", st.ime, st.prezime, st.brIndeksa, &st.brIspita);
-        addBegin(l, st);
+        addEnd(l, st);
     }
 
-    // ispis studenata
-    node *t = l->next;
+    // ispis studenata iz kruzne liste
+    node* t = l->next;
     while (t != l) {
         printf("%s\n", t->data.ime);
         printf("%s\n", t->data.prezime);
