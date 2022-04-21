d2c832d98effc2e9951fdfa67ee717aa231e2c7c
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-21 22:52:42 +0200

    Update zadatak1.c

--- v03/zadatak1.c
+++ v03/zadatak1.c
@@ -1,17 +1,26 @@
+/*
+Napraviti sledeće funkcije:
+void copyLEToLE(int, void*) - koja kopira broj dat u LE formatu na datu adresu u LE formatu
+void copyLEToBE(int, void*) - koja kopira broj dat u LE formatu na datu adresu u BE formatu
+
+hint: za kopiranje iz BE u LE ne moramo praviti novu funkciju, koristimo LE u BE u oba slučaja.
+fun fact: za float nam ne treba nova funkcija, nego samo float tretiramo kao int
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
-void copyLEToLE(int x, void *copy) {
-   for(int i = 0; i < sizeof(x); i++) {
-      *((char *)copy + i) = *((char *)&x + i);
-   }
+void copyLEToLE(int x, void* copy) {
+	for(int i = 0; i < sizeof(x); i++) {
+    	*((char*) copy + i) = *((char*) &x + i);
+	}
 }
 
-void copyLEToBE(int x, void *copy) {
-   for(int i = 0; i < sizeof(x); i++) {
-      *((char *)copy + i) = *((char *)&x + sizeof(x) - 1 - i);
-   }
+void copyLEToBE(int x, void* copy) {
+	for(int i = 0; i < sizeof(x); i++) {
+		*((char*) copy + i) = *((char*) &x + sizeof(x) - 1 - i);
+	}
 }
 
 int main()
