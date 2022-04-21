21adf991c6fea8bedb3ad7eeec21dd3540b68b00
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-21 22:52:11 +0200

    Update zadatak2.c

--- v02/zadatak2.c
+++ v02/zadatak2.c
@@ -1,38 +1,50 @@
+/*
+Napisati program koji učitava broj N (N > 0) tipa short i niz od N brojeva tipa integer, 
+pakuje broj N i unete brojeve u niz bajtova i prosleđuje dobijeni niz funkciji average() 
+koja izračunava i vraća najmanju i najveću vrednost u nizu.
+
+Zaglavlje funkcije average() dato je ispod:
+int* CalculateMinMax(char* buffer);
+
+Povratna vrednost funkcije je pokazivač na niz od dva podatka tipa integer koji 
+predstavljaju najmanju i najveću vrednost u nizu.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int * CalculateMinMax(char *buffer) {
+int* CalculateMinMax(char* buffer) {
 
-	int *rez = (int *)malloc(2 * sizeof(int));
-	int N = (int)(*buffer);
-	int *niz = (int *)(buffer + sizeof(char));
+	int* rez = (int*) malloc(2 * sizeof(int));
+	int N = (int) (*buffer);
+	int* niz = (int*) (buffer + sizeof(char));
 
 	rez[0] = niz[0];		// min
 	rez[1] = niz[0];		// max
 
 	// prolaz kroz niz, racunanje min i max
 	for (int i = 1; i < N; i++) {
-		if (niz[i] < rez[0]) {
+		if (niz[i] < rez[0])
 			rez[0] = niz[i];
-		}
-		if (niz[i] > rez[1]) {
+		if (niz[i] > rez[1])
 			rez[1] = niz[i];
-		}
 	}
 
 	return rez;
 }
 
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	short n;		// N>0 koji se unosi
-	printf("Unesi broj N > 0: ");
+	// N > 0 koji se unosi
+	short n;			
+	printf("Unesi broj N (N > 0): ");
 	scanf_s("%hi", &n);
 
 	printf("\n");
 
-	int *niz = (int *)malloc(n * sizeof(int));
+	// inicijalizacija i unos niza
+	int* niz = (int*) malloc(n * sizeof(int));
 
 	printf("Unos niza:\n");
 	for (int i = 0; i < n; i++) {
@@ -41,11 +53,11 @@ int main(int argc, char *argv[])
 	}
 
 	// spakovati broj N i unete brojeve u niz bajtova u buffer (koji je niz char-ova)
-	char *buffer = (char *)malloc(n * sizeof(int) + sizeof(char));
-	buffer[0] = (char)n;
+	char* buffer = (char*) malloc(n * sizeof(int) + sizeof(char));
+	buffer[0] = (char) n;
 
 	// pokazivač *brojevi pokazuje na drugi bajt u bufferu i odatle do kraja upisuje brojeve iz niza
-	int *brojevi = (int *)(buffer + 1 * sizeof(char));
+	int* brojevi = (int*) (buffer + 1 * sizeof(char));
 	for (int i = 0; i < n; i++) {
 		brojevi[i] = niz[i];
 	}
@@ -53,7 +65,8 @@ int main(int argc, char *argv[])
 	// niz nam vise ne treba, sve je u bufferu
 	free(niz);
 
-	int *rezultat = CalculateMinMax(buffer);
+	// poziv funkcije
+	int* rezultat = CalculateMinMax(buffer);
 
 	printf("\n");
 
