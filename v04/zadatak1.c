4f7d279d88e43731b9799c030d86f1550c0c6ca0
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-21 22:55:41 +0200

    Update zadatak1.c

--- v04/zadatak1.c
+++ v04/zadatak1.c
@@ -1,48 +1,39 @@
 /*
-Napisati program koji učitava broj X tipa int i konvertuje ga i binarni sistem u obliku stringa.
+Napisati program koji učitava broj V u binarnom sistemu u obliku stringa i konvertuje taj broj u tip podataka int.
 
 Program treba da ima funkciju sa zaglavljem:
-char* konverzija(int x);
+int konverzije(char* s);
 
-Funkciju realizovati koristeći matematičke operacije / i % da bi se dobio traženi broj.
+Funkciju realizovati koristeći matematičke operacije + i * da bi se dobio traženi broj.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-char* konverzija(int n){
-	char s[33], *rez;
-	s[32] = 0;
-	int i = 31;
+int konverzija(char* s){
+	int rez, i;
+   	rez = 0;
+   	i = 0;
 
-	do {
-    	if (n % 2 == 0) {
-    		s[i] = '0';
-		}
-    	else {
-        	s[i] = '1';
-		}
-
-     	i--;
-     	n = n / 2;
-   	} while (n != 0);
-
-   	rez = (char*) malloc(32 - i);
-   	memcpy(rez, &s[i + 1], 32 - i);
-
+	// ascii 0 - \0 (NULL)
+   	while (s[i] != 0) {
+    	rez = rez * 2 + s[i] - '0';
+    	i++;
+   	}
+	   
    	return rez;
 }
 
 int main()
 {
     int n;
-    char* s;
+    char s[20];
 
-    printf("Unesite dekadni broj koji treba kovertovati: ");
-    scanf("%d", &n);
-	
-    s = konverzija(n);
-    printf("Binarni broj izgleda ovako: %s\n", s);
+    printf("Unesite binarni broj koji treba kovertovati: ");
+    scanf("%s", &s);
+
+    n = konverzija(s);
+    printf("Dekadni broj izgleda ovako: %d\n", n);
 	
     return 0;
 }
