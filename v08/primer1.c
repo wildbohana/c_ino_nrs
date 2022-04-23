a3da3e9971c040ef375150ceb033c65503902b89
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-23 20:39:50 +0200

    Update primer1.c

--- v08/primer1.c
+++ v08/primer1.c
@@ -15,44 +15,48 @@ typedef int poredjenje(int, int);
 
 typedef int poredjenje(int, int);
 
-int uporediManje(int a, int b) {
+int uporediManje(int a, int b) 
+{
 	return a < b;
 }
 
-int uporediVece(int a, int b) {
+int uporediVece(int a, int b) 
+{
 	return a > b;
 }
 
-void bubbleSort(int a[], int n, poredjenje *uporedi) {
-	int i, j, t;
-	for (j = n - 1; j > 0; j--)
-    	for (i = 0; i < j; i++)
-        	if ((*uporedi)(a[i + 1], a[i])) {
-				t = a[i];
-				a[i] = a[i + 1];
-				a[i + 1] = t;
-			}
+void bubbleSort(int a[], int n, poredjenje* uporedi) 
+{
+    int i, j, t;
+    for (j = n - 1; j > 0; j--)
+        for (i = 0; i < j; i++)
+            if ((*uporedi)(a[i + 1], a[i])) 
+	    {
+		t = a[i];
+		a[i] = a[i + 1];
+		a[i + 1] = t;
+	    }
 }
 
 int main()
 {
-	// unosimo broj n
+    // unosimo broj n
     int n;
     printf("Unesi broj n: ");
     scanf("%d", &n);
 
-	// unosimo niz
+    // unosimo niz
     int i, a[n];
     for (i = 0; i < n; i++)
         scanf("%d", &a[i]);
 
-	// sortirano rastuće
+    // sortirano rastuće
     bubbleSort(a, n, uporediManje);
     printf("\nNiz sortiran rastuce izgleda ovako: \n");
     for (i = 0; i < n; i++)
         printf("%d ", a[i]);
 
-	// sortirano opadajuće
+    // sortirano opadajuće
     bubbleSort(a, n, uporediVece);
     printf("\nNiz sortiran opadajuce izgleda ovako: \n");
     for (i = 0; i < n; i++)
