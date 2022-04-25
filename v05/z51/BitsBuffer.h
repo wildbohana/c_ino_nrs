4c3583cb6a9012ca746d0ddef80907ce9a84af82
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-25 02:01:15 +0200

    Add files via upload

---
+++ v05/z51/BitsBuffer.h
@@ -0,0 +1,26 @@
+#ifndef BITSBUFFER_H_INCLUDED
+#define BITSBUFFER_H_INCLUDED
+
+#define size 1024
+#define bitsize 8*size
+
+typedef struct {
+    char buffer[size];
+    int count, first, last;
+} bitbuffer;
+
+void initbuff(bitbuffer* b);
+
+int isEmpty(bitbuffer b);
+
+int isFull(bitbuffer b);
+
+void setBit(char* ch, int bitindex, int num);
+
+int isSetBit(char ch, int bitindex);
+
+void push(bitbuffer* b, int num);
+
+int pop(bitbuffer* b);
+
+#endif
