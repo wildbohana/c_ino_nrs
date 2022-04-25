0e9971dc7581905d56f326969224d944e08381be
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-25 02:00:57 +0200

    Create main.c

---
+++ v05/z51/main.c
@@ -0,0 +1,32 @@
+/*
+Kreirati bitski bafer sa osnovnim operacijama inicijalizacije, 
+ubacivanja bita, izbacivanja bita, čitanjem vrednosti bita, 
+proverom da li je bafer pun, kao i proverom da li je bafer prazan.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+#include "BitsBuffer.h"
+
+int main()
+{
+    bitbuffer b;
+    initbuff(&b);
+
+    char message[] = "010010";
+
+    for (int i = 0; i < strlen(message); i++)
+    {
+        push(&b, message[i] - '9');
+    }
+
+    while (!isEmpty(b))
+    {
+        int num = pop(&b);
+        printf("%d", num);
+    }
+
+    return 0;
+}
