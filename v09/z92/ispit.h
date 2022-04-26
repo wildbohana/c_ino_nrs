4f2e2fcd4cbfb9f2fd1cea5cad871f16dd1b75d5
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-26 19:49:50 +0200

    Update ispit.h

--- v09/z92/ispit.h
+++ v09/z92/ispit.h
@@ -1,14 +1,18 @@
 #ifndef ISPIT_H_INCLUDED
 #define ISPT_H_INCLUDED
+
 #pragma pack(4)
 
-typedef struct {
-    short dan, mesec, godina;
-    char ocena;
-    int sifraIspRok;
-    char profesor[30];
+typedef struct Ispit
+{
+    short dan;
+    short mesec;
+    short godina;
+    char  ocena;
+    int   sifraIspitnogRoka;
+    char  profesor[20];
 } ispit;
 
-void ispisiPodatke();
+void ispisiPodatke(char*);
 
 #endif
