f30136c32c2b3ad05d9c1c4a45943a846fe8dba3
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-26 19:52:50 +0200

    Update main.c

--- v02/z22/main.c
+++ v02/z22/main.c
@@ -1,30 +1,18 @@
-/*
-Napisati program koji učitava broj N (N > 0) tipa short i niz od N brojeva tipa integer, 
-pakuje broj N i unete brojeve u niz bajtova i prosleđuje dobijeni niz funkciji average() 
-koja izračunava i vraća najmanju i najveću vrednost u nizu.
-
-Zaglavlje funkcije average() dato je ispod:
-int* CalculateMinMax(char* buffer);
-
-Povratna vrednost funkcije je pokazivač na niz od dva podatka tipa integer koji 
-predstavljaju najmanju i najveću vrednost u nizu.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int* CalculateMinMax(char* buffer)
 {
     int* rez = (int*) malloc(2 * sizeof(int));
-    int n = (int) buffer[0];
-
-    // pokazivac na pocetak niza
     int* niz = (int*) (buffer + sizeof(char));
 
-    rez[0] = niz[0];        // min
-    rez[1] = niz[0];        // max
+    int n = (int) buffer[0];
 
-    for (int i = 0; i < n; i++) {
+    rez[0] = niz[0];    // min
+    rez[1] = niz[0];    // max
+
+    for (int i = 1; i < n; i++)
+    {
         if (niz[i] < rez[0])
             rez[0] = niz[i];
         if (niz[i] > rez[1])
@@ -36,43 +24,37 @@ int* CalculateMinMax(char* buffer)
 
 int main()
 {
-    short n = -1;
+    short n;
 
     do {
-        printf("Unesite broj N (N>0): ");
+        printf("Unesite broj n (n>0): ");
         scanf("%hi", &n);
     } while (n <= 0);
 
+    // najbolje dinamicki niz ovde da se koristi
     int* niz = (int*) malloc(n * sizeof(int));
 
-    for (int i = 0; i < n; i++) {
-        printf("niz[%d] = ", i);
+    for (int i = 0; i < n; i++)
+    {
+        printf("niz[%d]: ", i);
         scanf("%d", &niz[i]);
     }
 
     char* bafer = (char*) malloc(sizeof(char) + n * sizeof(int));
-    bafer[0] = n;
 
-    // pokazivac na elemente iz bafera
-    int* brojevi = (int*) (bafer + 1 * sizeof(char));
+    bafer[0] = (char) n;
+    int* brojevi = (int*) (bafer + sizeof(char));
 
-    // ovde se preko brojeva posredno pristupa brojevima iz bafera
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         brojevi[i] = niz[i];
-    }
 
-    // niz nam vise ne treba
+    int* MiniMax = CalculateMinMax(bafer);
+    printf("\nMinimum je: %d", MiniMax[0]);
+    printf("\nMaksimum je: %d", MiniMax[1]);
+
+    // oslobodi dinamicki alociranu memoriju
     free(niz);
-
-    int* rezultat = CalculateMinMax(bafer);
-
-    printf("\n");
-
-    printf("Min je: %d\n", rezultat[0]);
-    printf("Max je: %d\n", rezultat[1]);
-
-    // zbog malloca, YOU FORGOT AGAIN
-    free(rezultat);
+    free(bafer);
 
     return 0;
 }
