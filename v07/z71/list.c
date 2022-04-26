8e9b9ef7a0cf47008b49cdf87ac655ddff364906
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-26 19:59:11 +0200

    Update list.c

--- v07/z71/list.c
+++ v07/z71/list.c
@@ -1,29 +1,31 @@
 #include "list.h"
 #include <stdlib.h>
 
-// inicijalizacija
+// cvor l je granicnik u ovoj listi
+// cvor l je prazan
+
 void init(node **l)
 {
-	*l = (node*) malloc(sizeof(node));
-	(*l)->next = *l;
-};
+    *l = (node*) malloc(sizeof(node));
+    (*l) -> next = *l;  // sam na sebe pokazuje
+}
 
-// dodavanje na pocetak
+
 void addBegin(node *l, student s)
 {
-	node *temp = (node*) malloc(sizeof(node));
-	temp->data = s;
-	temp->next = l->next;
-	l->next = temp;
-};
+    node* temp = (node*) malloc(sizeof(node));
+    temp -> data = s;
+    temp -> next = l -> next;
+    l -> next = temp;   // pokazuje na novododati
+}
 
-// dodavanje na kraj - dodajemo podatak u granicnik
 void addEnd(node **l, student s)
 {
-	node *temp = (node*) malloc(sizeof(node));
-	(*l)->data = s;
-	temp->next = (*l)->next;
-	(*l)->next = temp;
-	*l = temp;
-};
+    node* temp = (node*) malloc(sizeof(node));
+    (*l) -> data = s;   // granicnik dobija podatke
 
+    temp -> next = (*l) -> next;
+    (*l) -> next = temp;
+
+    *l = temp;          // novi granicnik je novododati
+}
