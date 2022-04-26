91f39e6ddecc64f82c4a88e7a33837d4d45dd14b
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-26 19:50:53 +0200

    Update izraz.c

--- v09/z91/izraz.c
+++ v09/z91/izraz.c
@@ -3,23 +3,23 @@
 
 void ispisNiz(char* buffer)
 {
-	int n = *((int*) buffer);
+	int n = (int*) buffer[0];
 	int i;
-	izraz *niz = (izraz*)(buffer + sizeof(int));
 
-	for(i = 0; i < n; i++)
-		printf("%d %c %d = %d\n",
-         niz[i].broj1,
-         niz[i].op,
-         niz[i].broj2,
-         niz[i].rezultat);
+	izraz* niz = (izraz*) (buffer + sizeof(int));
+
+	for (i = 0; i < n; i++)
+        printf("%d %c %d = %d\n",
+        niz[i].broj1, niz[i].op, niz[i].broj2, niz[i].rezultat);
 };
 
 char* napraviNiz()
 {
 	char* rez = (char*) malloc(4 * sizeof(izraz) + sizeof(int));
+
 	izraz* niz = (izraz*) (rez + sizeof(int));
 
+	// po srpski???
 	*((int*) rez) = 4;
 
 	niz[0] = (izraz) {11, '+', 4, 0};
