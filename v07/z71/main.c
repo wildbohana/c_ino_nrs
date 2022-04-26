b7e0aadd5bc88c8fde05a9d634e36784d35bd86f
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-26 19:58:35 +0200

    Update main.c

--- v07/z71/main.c
+++ v07/z71/main.c
@@ -1,51 +1,37 @@
-/*
-Napraviti listu koja sadrži podatke o N studenata i smestiti kreiranu listu u bafer.
-Za svakog studenta zapamtiti ime, prezime, broj indeksa i broj položenih ispita.
-Napisati funkciju izdvoj() koja iz datog bafera izdvaja sve studente koji su položili više od 5 ispita.
-
-Zaglavlje funkcije dato je na sledeći način:
-list izdvoji(char* buffer);
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "list.h"
 
-//list izdvoj(char* buffer);
-
 int main()
 {
-    node* l;
+    node *l;
     init(&l);
 
-    student st;
     int n;
-
     printf("Unesite broj studenata: ");
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
-        printf("Unesite podatke o studentu broj %d: \n", i+1);
-        scanf("%s", st.ime);
-        scanf("%s", st.prezime);
-        scanf("%s", st.brIndeksa);
-        scanf("%d", &st.brIspita);
+    student st;
 
-        addBegin(l, st);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Unesite podatke o studentu broj %d: \n", i+1);
+        scanf("%s%s%s%d", st.ime, st.prezime, st.brIndeksa, &st.brIspita);
+
+        addEnd(&l, st);
+        // addBegin(l, st);
     }
 
-    printf("\n");
+    node* temp = l -> next;
 
-    node* t = l->next;
-
-    while (t != l) {
-        printf("%s\n", t->data.ime);
-        printf("%s\n", t->data.prezime);
-        printf("%s\n", t->data.brIndeksa);
-        printf("%d\n", t->data.brIspita);
-
-        t = t->next;
+    while (temp != l)
+    {
+        printf("%s\n", temp -> data.ime);
+        printf("%s\n", temp -> data.prezime);
+        printf("%s\n", temp -> data.brIndeksa);
+        printf("%d\n", temp -> data.brIspita);
+        temp = temp -> next;
     }
 
     return 0;
