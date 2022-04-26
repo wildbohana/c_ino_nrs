622f5c64621e631c83d633e8e09c7303d463aeb1
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-26 19:53:33 +0200

    Update main.c

--- v03/z32/main.c
+++ v03/z32/main.c
@@ -1,22 +1,48 @@
-/*
-Koristeći kreirane funkcije, kreirati funkciju Calculate, koja prima bafer od 10 bajtova, od čega:
-a) nulti bajt predstavlja slovo 'L' ili 'B', koji označava da li je broj dat u LE ili BE formatu
-b) prvi bajt predstavlja operaciju (+, -, *, /)
-c) preostalih 8 bajtova predstavljaju dva broja tipa integer, nad kojim je potrebno realizovati operaciju.
-
-U slučaju da je nulti bajt 'L', brojevi su dati u LE formatu, a ako je nulti bajt 'B', broj je dat u BE formatu.
-Povratna vrednost funkcije je tip podataka float koji sadrži rezultat u istom formatu.
-
-Kreirati primer koji demonstrira rad kreiranih funkcija.
-
-Zaglavlje funkcije Calculate dato je sa:
-float Calculate(char* buffer);
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
-// #include <ctype.h>
+#include <ctype.h>
+
+float Calculate(char* buffer)
+{
+    float rez;
+
+    char c = buffer[0];
+    char o = buffer[1];
+
+    int op1;
+    int op2;
+
+    // prva konverzija
+    if (c == 'L')
+    {
+        copyLEtoLE(*((int*) (buffer + 2)), &op1);
+        copyLEtoLE(*((int*) (buffer + 6)), &op2);
+    }
+    else
+    {
+        copyLEtoBE(*((int*) (buffer + 2)), &op1);
+        copyLEtoBE(*((int*) (buffer + 6)), &op2);
+    }
+
+    // racunanje
+    switch (o)
+    {
+        case '+': rez = (float) op1 + op2; break;
+        case '-': rez = (float) op1 - op2; break;
+        case '*': rez = (float) op1 * op2; break;
+        case '/': rez = (float) op1 / op2; break;
+    }
+
+    // druga konverzija (ako je potrebno)
+    if (c == 'L')
+        return rez;
+    else
+    {
+        copyLEtoBE(*((int*) &rez), &rez);
+        return rez;
+    }
+}
 
 void copyLEtoLE(int x, void* copy)
 {
@@ -28,97 +54,59 @@ void copyLEtoBE(int x, void* copy)
 {
     for (int i = 0; i < sizeof(x); i++)
         *((char*) copy + i) = *((char*) &x + sizeof(x) - 1 - i);
-}
 
-float Calculate(char* buffer)
-{
-    char format = buffer[0];
-    char operacija = buffer[1];
-
-    int op1, op2;
-    float rez;
-
-    if (format == 'L')
-    {
-        copyLEtoLE(*((int*) (buffer + 2)), &op1);
-        copyLEtoLE(*((int*) (buffer + 6)), &op2);
-    }
-    else
-    {
-        copyLEtoBE(*((int*) (buffer + 2)), &op1);
-        copyLEtoBE(*((int*) (buffer + 6)), &op2);
-    }
-
-    switch (operacija)
-    {
-        case '+':
-            rez = op1 + op2; break;
-        case '-':
-            rez = op1 - op2; break;
-        case '*':
-            rez = op1 * op2; break;
-        case '/':
-            rez = op1 / op2; break;
-    }
-
-    if (format == 'L')
-        return rez;
-    else
-    {
-        copyLEtoBE(*((int*) &rez), &rez);
-        return rez;
-    }
 }
 
 int main()
 {
-    char format, operacija;
-    int x, y;
-    float rez;
+    char f;
+    char op;
 
-    char bafer[2 * sizeof(char) + 2 * sizeof(int)];
+    char* bafer = (char*) malloc(10 * sizeof(char));
 
-    do {
-        printf("Unesite format zapisa brojeva [L/B]: ");
-        scanf("%c", &format);
-        fflush(stdin);
-    } while (toupper(format) != 'L' && toupper(format) != 'B');
+    int op1;
+    int op2;
 
-    do {
-        printf("Unesite operaciju [+, -, *, /]: ");
-        scanf("%c", &operacija);
-        fflush(stdin);
-    } while (operacija != '+' && operacija != '-' && operacija != '*' && operacija != '/');
+    printf("Izaberite format [L/B]: ");
+    scanf("%c", &f);
+    fflush(stdin);
 
-    printf("Unesite prvi operand: ");
-    scanf("%d", &x);
-    printf("Unesite drugi operand: ");
-    scanf("%d", &y);
+    printf("Izaberite operaciju [+, -, *, /]: ");
+    scanf("%c", &op);
+    fflush(stdin);
 
-    if (toupper(format) == 'L')
+    printf("Unesi prvi operand: ");
+    scanf("%d", &op1);
+    printf("Unesi drugi operand: ");
+    scanf("%d", &op2);
+
+    if (toupper(f) == 'L')
     {
         bafer[0] = 'L';
-        bafer[1] = operacija;
+        bafer[1] = op;
 
-        copyLEtoLE(x, bafer + 2 * sizeof(char));
-        copyLEtoLE(y, bafer + 2 * sizeof(char) + sizeof(int));
+        copyLEtoLE(op1, bafer + 2);
+        copyLEtoLE(op2, bafer + 6);
 
-        rez = Calculate(bafer);
+        float rezultat = Calculate(bafer);
+
+        printf("Rezultat: %f", rezultat);
     }
     else
     {
         bafer[0] = 'B';
-        bafer[1] = operacija;
+        bafer[1] = op;
 
-        copyLEtoBE(x, bafer + 2 * sizeof(char));
-        copyLEtoBE(y, bafer + 2 * sizeof(char) + sizeof(int));
+        copyLEtoBE(op1, bafer + 2);
+        copyLEtoBE(op2, bafer + 6);
 
-        rez = Calculate(bafer);
+        float rezultat = Calculate(bafer);
 
-        copyLEtoBE(*((int*) &rez), &rez);
+        copyLEtoBE(*((int*) & rezultat), &rezultat);
+        printf("Rezultat: %f", rezultat);
     }
 
-    printf("\nRezultat je: %f", rez);
+    free(bafer);
 
     return 0;
 }
