a95a44b04e7ce268685129d592c1a7cdf27a6b2a
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-04-28 11:04:54 +0200

    Update main.c

--- v09/z92/main.c
+++ v09/z92/main.c
@@ -24,13 +24,119 @@ typedef struct {
 #include <stdlib.h>
 
 #include "ispit.h"
+
 #pragma pack(4)
 
-int main()
+int konverzija(char* string)
 {
-    char* buffer;
+    int rez = 0;
+    int i = 0;
 
-    //ispisiPodatke(buffer);
+    while(string[i] != 0)
+    {
+        if (string[i] >= '0' && string[i] <= '9')
+            rez = rez * 16 + string[i] - '0';
+        else if (string[i] >= 'A' && string[i] <= 'F')
+            rez = rez * 16 + string[i] - 'A' + 10;
+        else if (string[i] >= 'a' && string[i] <= 'f')
+            rez = rez * 16 + string[i] - 'a' + 10;
+        else
+            return -1;
+
+        i++;
+    }
+
+    return rez;
+}
+
+char* ucitavanjePodatakaIUpisUBuffer()
+{
+    int brojPredmeta;
+    int brojStudenata;
+
+    printf("\nUnesite broj predmeta: ");
+    scanf("%d", &brojPredmeta);
+    printf("\nUnesite broj studenata: ");
+    scanf("%d", &brojStudenata);
+
+    char* buffer = (char*) malloc(2 * sizeof(int) + brojPredmeta * brojStudenata * sizeof(ispit));
+
+    *((int*) buffer) = brojStudenata;
+    *((int*) (buffer + sizeof(int))) = brojPredmeta;
+
+    for (int i = 0; i < brojPredmeta; i++)
+    {
+        for (int j = 0; j < brojStudenata; j++)
+        {
+            int hexSifra  = -1;
+
+            short* dan = ((short*) (buffer + 2 * sizeof(int) + (i * brojStudenata + j) * sizeof(ispit)));
+            short* mesec = ((short*) (buffer + 2 * sizeof(int) + (i * brojStudenata + j) * sizeof(ispit) + 2));
+            short* godina = ((short*) (buffer + 2 * sizeof(int) + (i * brojStudenata + j) * sizeof(ispit) + 4));
+            char* ocena = ((char*) (buffer + 2 * sizeof(int) + (i * brojStudenata + j) * sizeof(ispit) + 6));
+            int* sifra = ((int*) (buffer + 2 * sizeof(int) + (i * brojStudenata + j) * sizeof(ispit) + 8));
+            char* prof = ((char*) (buffer + 2 * sizeof(int) + (i * brojStudenata + j) * sizeof(ispit) + 12));
+
+            printf("\nStudent[%d]", i + 1);
+
+            printf("\n\tDan, Mesec, Godina: ");
+            scanf("%hd %hd %hd", dan, mesec, godina);
+
+            printf("\tOcena: ");
+            scanf("%c", ocena);
+
+            do {
+                char sifraUnos[12];
+
+                printf("\tSifra Ispitnog Roka: ");
+                fflush(stdin);
+
+                scanf(" %s", sifraUnos);
+                printf("\n");
+
+                hexSifra = konverzija(sifraUnos);
+            } while (hexSifra == -1);
+
+            *sifra = hexSifra;
+
+            printf("\tProfesor: ");
+            scanf("%s", prof);
+        }
+    }
+
+    return buffer;
+}
+
+typedef int polozen(ispit);
+
+int polozio(ispit isp)
+{
+    return isp.ocena > '5';
+}
+
+int prebroj(char* buffer, polozen p)
+{
+    int brojStudenata = *((int *) buffer);
+    int brojPredmeta = *((int *)(buffer + sizeof(int)));
+
+    int brojac = 0;
+
+    ispit* isp = (ispit*) (buffer + 2 * sizeof(int));
+
+    for (int j = 0; j < brojPredmeta; j++)
+        for (int i = 0; i < brojStudenata; i++)
+            if (p(isp[j * brojStudenata + i]))
+                brojac++;
+
+    return brojac;
+}
+
+int main(void)
+{
+    char* buffer = ucitavanjePodatakaIUpisUBuffer();
+
+    ispisiPodatke(buffer);
+    printf("\nBroj ispita koji zadovoljavaju uslov: %d ispit/a.\n", prebroj(buffer, polozio));
 
     return 0;
 }
