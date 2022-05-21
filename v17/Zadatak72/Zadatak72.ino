2391d863e0ec63af7f89f5b87c2f8f26d7bb6286
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-21 08:47:32 +0200

    Update Zadatak72.ino

--- v17/Zadatak72/Zadatak72.ino
+++ v17/Zadatak72/Zadatak72.ino
@@ -7,55 +7,71 @@ int PIN_SW1 = 2;
 int INT_SW1 = 1;
 int br, dugme_id;
 
-void prikazi(){
-   int sensorValue = analogRead(analogInPin);
-   int outputValue = map(sensorValue, 0, 1023, 0, 255);
-   for(int i=26; i<34; i++){
-      digitalWrite(i, outputValue%2);
-      outputValue = outputValue/2;
-   }
+void prikazi()
+{
+	int sensorValue = analogRead(analogInPin);
+	int outputValue = map(sensorValue, 0, 1023, 0, 255);
+
+	for (int i = 26; i < 34; i++)
+	{
+		digitalWrite(i, outputValue % 2);
+		outputValue = outputValue / 2;
+	}
 }
 
 void dugme(int id, void * tptr)
 {
     int newState = digitalRead(PIN_BTN1);
-  if (oldState == 1  && newState == 0) {
-        if (millis()-oldTime>=2000) {
-            int sensorValue = analogRead(analogInPin);
-            br = br+sensorValue;
-            Serial.println(br);
-    }
-  } else if (oldState == 0 && newState ==1) {
-        oldTime = millis();
-  }
-  oldState = newState;
+
+	if (oldState == 1 && newState == 0) 
+	{
+		if (millis() - oldTime >= 2000) 
+		{
+			int sensorValue = analogRead(analogInPin);
+			br = br + sensorValue;
+			Serial.println(br);
+		}
+	} 
+	else if (oldState == 0 && newState == 1) 
+	{
+		oldTime = millis();
+	}
+
+	oldState = newState;
 }
 
 void obrisiInterrupt(int id, void * tptr)
 {
-   if (Serial.available()){
-      char *s = Serial.readString();
-      // na plocici pisati
-      //const char* s = Serial.readString().c_str();
-      int x = atoi(s);
-      if (x<br) {
-        setTaskState(dugme_id, TASK_DISABLE);
-        Serial.println("Task onemogucen");
-      } else
-        Serial.println("Task nije onemogucen");
-   }
+	if (Serial.available())
+	{
+		// na plocici pisati
+		//const char* s = Serial.readString().c_str();
+		char *s = Serial.readString();
+		int x = atoi(s);
+
+		if (x < br) 
+		{
+			setTaskState(dugme_id, TASK_DISABLE);
+			Serial.println("Task onemogucen");
+		} 
+		else
+		{
+			Serial.println("Task nije onemogucen");
+		}
+	}
 }
 
 void setup()
 {
-  Serial.begin(9600);
-  for(int i = 0; i<8; i++)
-    pinMode(PIN_LD1+i, OUTPUT);
-  br = 0;
-  oldTime = millis();
-  dugme_id = createTask(dugme, 50, TASK_ENABLE, NULL);
-  attachInterrupt(INT_SW1, prikazi,RISING);
-  createTask(obrisiInterrupt, 50, TASK_ENABLE, NULL);
+	Serial.begin(9600);
+	for(int i = 0; i < 8; i++) pinMode(PIN_LD1 + i, OUTPUT);
+
+	br = 0;
+	oldTime = millis();
+
+	dugme_id = createTask(dugme, 50, TASK_ENABLE, NULL);
+	attachInterrupt(INT_SW1, prikazi,RISING);
+	createTask(obrisiInterrupt, 50, TASK_ENABLE, NULL);
 }
 
 void loop()
