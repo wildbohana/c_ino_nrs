0da9e0b261bb220968b70d6799a6ea6d8e5c608b
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-25 22:41:28 +0200

    Update Zadatak11.ino

--- v11/Zadatak11/Zadatak11.ino
+++ v11/Zadatak11/Zadatak11.ino
@@ -1,25 +1,35 @@
+/*
+Napisati program koji pali i gasi led diode LD8 i LD2 na pinovima 33 i 27. 
+Pin 33 se i pali i gasi u jednom ciklusu loop-a, 
+a pin 27 se pali ili gasi u zavisnosti od stanja prekidača SW2 na pinu 7.
+*/
+
 #define dump(v) Serial.println(v) 
 
-void setup() 
-{
-	pinMode(33, OUTPUT);
-	pinMode(27, OUTPUT);
+#define LED8 33
+#define LED2 27
+#define SW2 7
 
-	Serial.begin(9600);  
+void setup()
+{
+	pinMode(LED8, OUTPUT);
+	pinMode(LED2, OUTPUT);
+
+	Serial.begin(9600);
 }
 
-void loop() 
+void loop()
 {
-	digitalWrite(33, HIGH);
-	delay(1000); 
-	digitalWrite(33, LOW);
+	digitalWrite(LED8, HIGH);
+	delay(1000);
+	digitalWrite(LED8, LOW);
 	delay(1000);
 
-	int pin7 = digitalRead(7);
+	int pin7 = digitalRead(SW2);
 	dump(pin7);
 
 	if (pin7)
-		digitalWrite(27, HIGH);
+		digitalWrite(LED2, HIGH);
 	else
-		digitalWrite(27, LOW);  
+		digitalWrite(LED2, LOW);
 }
