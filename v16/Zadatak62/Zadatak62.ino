5698a928dd391738d3991efa6af7ce786a0349c2
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-25 22:50:57 +0200

    Update Zadatak62.ino

--- v16/Zadatak62/Zadatak62.ino
+++ v16/Zadatak62/Zadatak62.ino
@@ -1,4 +1,11 @@
-void ispis(int id, void * tptr) 
+/*
+Napisati program koji učitava cifru po cifru sa serijskog ulaza i 
+ispisuje te cifre u binarnom sistemu na lampicama na pinovima 29, 28, 27 i 26.
+- Ukoliko je na primer ucitana cifra 5, ispisati 0101, tj. 
+  upalite lampice 28 i 26, a ostale ugasiti.
+*/
+
+void ispis(int id, void* tptr) 
 {
 	if (Serial.available()) 
 	{
@@ -10,6 +17,8 @@ void ispis(int id, void * tptr)
 
 			c = c - '0';
 			char s[5];
+
+			// itoa(integer, string, osnova)
 			itoa(c, s, 2);
 
 			Serial.println(s);
