2186561c6e63ed455fcd4de784f81d15d98fd736
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-25 22:49:29 +0200

    Update Zadatak54.ino

--- v15/Zadatak54/Zadatak54.ino
+++ v15/Zadatak54/Zadatak54.ino
@@ -1,41 +1,46 @@
-const int analogInPin = A0;
+/*
+Napisati program koji sa potenctiometra čita vrednost ulaza na svakih 
+100ms i proverava da li je došlo do naglog skoka ulaznog signala i o 
+tome obaveštava korisnika putem serijskog porta. Do naglog skoka dolazi 
+ukoliko se u poslednjih 5 merenja vrednost signala promenila za više od 200.
+*/
+const int analog_in = A0;
 
 int merenja[5] = {0};
-int x = 0;
+int brojac = 0;
 
-void potenciometar(int id, void * tptr)
+void potenciometar(int id, void* tptr)
 {
-    int novaJacina = analogRead(analogInPin);
-	merenja[x] = novaJacina;
+	int nova_jacina = analogRead(analog_in);
+	merenja[brojac] = nova_jacina;
 
-	int minimum = merenja[0];
 	int maksimum = merenja[0];
-	
+	int minimum = merenja[0];
+
 	for (int i = 1; i < 5; i++)
 	{
 		if (merenja[i] < minimum)
 			minimum = merenja[i];
-
+		
 		if (merenja[i] > maksimum)
 			maksimum = merenja[i];
 	}
 
-	if (maksimum - minimum > 200)
-	{
+	if (abs(maksimum - minimum) > 200)
 		Serial.println("Doslo je do naglog skoka!\n");
-		
-		for (int i = 0; i < 5; i++)
-			merenja[i] = novaJacina;
-	}
-   
-    // x = (x + 1) % 5;
-	if (++x == 5) x = 0;
+
+	// bro but like, this would be array of all the same values, we could have easily used just one variable for this ???
+	for (int i = 0; i < 5; i++)
+		merenja[i] = nova_jacina;
+
+    // brojac = (brojac + 1) % 5;
+	if (++brojac == 5) brojac = 0;
 }
 
 void setup()
 {
-    Serial.begin(9600);
-    createTask(potenciometar, 100, TASK_ENABLE, NULL);
+	Serial.begin(9600);
+	createTask(potenciometar, 100, TASK_ENABLE, NULL);
 }
 
 void loop()
