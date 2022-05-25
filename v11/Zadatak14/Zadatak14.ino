4f3542550274e396dbc923798087d6adb527db06
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-25 22:42:53 +0200

    Update Zadatak14.ino

--- v11/Zadatak14/Zadatak14.ino
+++ v11/Zadatak14/Zadatak14.ino
@@ -1,11 +1,17 @@
-#define LED_1 26
+/*
+Napisati program koji broji koliko je upaljenih prekidača 
+na pinovima 2, 7, 8 i 35, i u zavisnosti od toga pali duplo 
+veći broj lampica, počevši od lampice sa brojem 26.
+*/
+
+#define LED1 26
 
 void setup()
 {
     for (int i = 0; i < 8; i++)
     {
-        pinMode(LED_1 + i, OUTPUT);
-        digitalWrite(LED_1 + i, LOW);
+        pinMode(LED1 + i, OUTPUT);
+        digitalWrite(LED1 + i, LOW);
     }
 
     Serial.begin(9600);
@@ -13,27 +19,20 @@ void setup()
 
 void loop()
 {
-    int cnt = 0;
+    int brojac = 0;
 
-    int pin2  = digitalRead(2);
-    int pin7  = digitalRead(7);
-    int pin8  = digitalRead(8);
-    int pin35 = digitalRead(35);
+	if (digitalRead(2))  brojac++;
+	if (digitalRead(7))  brojac++;
+	if (digitalRead(8))  brojac++;
+	if (digitalRead(35)) brojac++;
 
-    int LED_NUM = 0;
+	int br_lampica = brojac * 2;
 
-    if (pin2)  cnt++;
-    if (pin7)  cnt++;
-    if (pin8)  cnt++;
-    if (pin35) cnt++;
+	for (int i = 0; i < br_lampica; i++)
+		digitalWrite(LED1 + i, HIGH);
+	
+	delay(1000);
 
-    LED_NUM = cnt * 2;
-
-    for (int i = 0; i < LED_NUM; i++)
-        digitalWrite(LED_1 + i, HIGH);
-
-    delay(1000);
-
-    for (int i = 0; i < 8; i++)
-        digitalWrite(LED_1 + i, LOW);
+	for (int i = 0; i < br_lampica; i++)
+		digitalWrite(LED1 + i, LOW);
 }
