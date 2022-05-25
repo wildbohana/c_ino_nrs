1b96340b608193ca8c7ac07011f26463254e02ae
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-25 22:50:27 +0200

    Update Zadatak61.ino

--- v16/Zadatak61/Zadatak61.ino
+++ v16/Zadatak61/Zadatak61.ino
@@ -1,4 +1,9 @@
-void kopiranje(int id, void * tptr) 
+/*
+Napisati program koji uzima jedan po jedan znak sa 
+serijskog ulaza i ispisuje ga na serijski izlaz.
+*/
+
+void kopiranje(int id, void* tptr) 
 {
    	if (Serial.available())
     	Serial.write(Serial.read());
