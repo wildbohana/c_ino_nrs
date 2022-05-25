4567df0417d4fc3b50f4501b80ac21cca2af3082
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-25 22:44:46 +0200

    Update Zadatak24.ino

--- v12/Zadatak24/Zadatak24.ino
+++ v12/Zadatak24/Zadatak24.ino
@@ -1,12 +1,22 @@
+/*
+Napisati program koji koristeći task blink_task1 proverava stanje 
+na prekidaču i ukoliko je prekidač upaljen, upaliti lampicu, 
+a ukoliko je prekidač ugašen, ugasiti lampicu. 
+Redni broj prekidača i lampice primiti kao parametar. 
+Koristeći dati task, kontrolisati lampice na pinovima 26, 27, 28 i 29 
+u zavisnosti od stanja na prikidačima na pinovima 2, 7, 8 i 35.
+*/
+
 typedef struct {
-    int led, sw;
+    int led;
+	int sw;
 } Struktura;
 
 Struktura s1, s2, s3, s4;
 
 void blink_task1(int id, void * tptr)
 {
-    Struktura *s = (Struktura*) tptr;
+    Struktura* s = (Struktura*) tptr;
 
     if (digitalRead(s->sw)) 
 		digitalWrite(s->led, HIGH);
