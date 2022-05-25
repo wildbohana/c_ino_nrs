f020176718c0d9e9a7fc9e4e245b352a4725ab74
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-05-25 22:52:01 +0200

    Update Zadatak64.ino

--- v16/Zadatak64/Zadatak64.ino
+++ v16/Zadatak64/Zadatak64.ino
@@ -1,12 +1,20 @@
-void ispis(int id, void * tptr) 
+/*
+Napisati program koji na svakih 1s učitava binarni broj koji 
+formiraju prekidaci na pinovima 25, 8, 7 i 2m prebacuje ga u 
+dekadni i ispisuje taj broj na serijskom monitoru u dekadnom sistemu.
+- Na primer, ako su upaljeni samo prekidači na pinovima 8 i 2, 
+  tada oni čine broj 0101 u osnovi 2, što je broj 5.
+*/
+
+void ispis(int id, void* tptr) 
 {
 	int broj = 0;
 
-	if (digitalRead(35)) broj += 8;
-	if (digitalRead(8))  broj += 4;
-	if (digitalRead(7))  broj += 2;
 	if (digitalRead(2))  broj += 1;
-   	
+	if (digitalRead(7))  broj += 2;
+	if (digitalRead(8))  broj += 4;
+	if (digitalRead(35)) broj += 8;
+  	
 	Serial.println(broj);
 }
 
