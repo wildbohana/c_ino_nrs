970ec781174e18ce8e12af064b0f973f493fb6c7
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-06-08 10:54:30 +0200

    Update demoZadatak104.cpp

--- v20/Zadatak104/demoZadatak104.cpp
+++ v20/Zadatak104/demoZadatak104.cpp
@@ -1,8 +1,8 @@
 /*
-Sa senzora se ucitavaju temperature vazduha u prostoriji u kojoj treba odrzavati 
-ujednacenu temperaturu od 20 stepeni. Ukoliko temperatura 3 puta zaredom 
-dostigne 21 stepen, ispisati na serijski monitor da treba upaliti klima 
-uredjaj na hladjenje. Ukoliko se temperatura 3 puta zaredom spusti ispod 19 
+Sa senzora se ucitavaju temperature vazduha u prostoriji u kojoj treba odrzavati
+ujednacenu temperaturu od 20 stepeni. Ukoliko temperatura 3 puta zaredom
+dostigne 21 stepen, ispisati na serijski monitor da treba upaliti klima
+uredjaj na hladjenje. Ukoliko se temperatura 3 puta zaredom spusti ispod 19
 stepeni, ispisati na serijski monitor da treba upaliti klima uredjaj na grejanje.
 */
 
@@ -33,16 +33,16 @@ void klima(int id, void* tptr)
 
 		values[numOfValues % numOfLastValues] = t;
 
-		if (numOfValues >= 3) 
+		if (numOfValues >= 3)
 		{
 			int prev1 = (numOfValues - 1) % numOfLastValues;
 			int prev2 = (numOfValues - 2) % numOfLastValues;
 
-			if (prev1 > optimum && prev2 > optimum && t > optimum)
+			if (values[prev1] > optimum && values[prev2] > optimum && t > optimum)
 			{
 				Serial.println("Postaje previse toplo. Ukljuci klimu.");
 			}
-			else if (prev1 < optimum && prev2 < optimum && t < optimum)
+			else if (values[prev1] < optimum && values[prev2] < optimum && t < optimum)
 			{
 				Serial.println("Postaje previse hladno. Iskljuci klimu.");
 			}
@@ -58,8 +58,8 @@ void setup()
 {
     Serial.begin(9600);
 
-    startStopDataGeneration(START_GENERATION, RANDOM, 0, 40, 0.2, 500);
-	
+    startStopDataGeneration(START_GENERATION, RANDOM, 15, 25, 0.1, 500);
+
 	numOfValues = 0;
     createTask(klima, 50, TASK_ENABLE, NULL);
 }
