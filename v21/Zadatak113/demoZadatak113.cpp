a65a7e0404a9d391f4bc67009f7b049fe11f0156
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-06-10 11:58:13 +0200

    Update and rename demoZadatak113Student.cpp to demoZadatak113.cpp

--- v21/Zadatak113/demoZadatak113.cpp
+++ v21/Zadatak113/demoZadatak113.cpp
@@ -24,11 +24,12 @@ Napomena 2: fajl data.cpp za ovaj zadatak nalazi se na sajtu predmeta.
 
 extern serial Serial;
 
-#define maxBrVrednosti 10
-int brojVrednosti = 0;
-int redniBrojGreske = 0;
-int Uredjaj[maxBrVrednosti];
-short Napon[maxBrVrednosti], Struja[maxBrVrednosti];
+#define brPoslednjihVrednosti 10
+int brVrednosti;
+int redniBrGreske;
+int uredjaj[brPoslednjihVrednosti];
+short napon[brPoslednjihVrednosti];
+short struja[brPoslednjihVrednosti];
 
 void brojevi(int id, void* tptr)
 {
@@ -37,41 +38,39 @@ void brojevi(int id, void* tptr)
         char* buffer = readAll();
         int velicina = *((int*) buffer);
 
+		brVrednosti++;
+
         for (int i = 0; i < velicina; i++)
         {
-            Uredjaj[brojVrednosti % maxBrVrednosti] = *(int*)(buffer + sizeof(int) + i * slogSize() + 32);
-            Napon[brojVrednosti % maxBrVrednosti]= *(short*)(buffer + sizeof(int) + i * slogSize() + 36);
-            Struja[brojVrednosti % maxBrVrednosti] = *(short*)(buffer + sizeof(int) + i * slogSize() + 38);
+            uredjaj[brVrednosti % brPoslednjihVrednosti] = *(int*)(buffer + sizeof(int) + i * slogSize() + 32);
+            napon[brVrednosti % brPoslednjihVrednosti] = *(short*)(buffer + sizeof(int) + i * slogSize() + 36);
+            struja[brVrednosti % brPoslednjihVrednosti] = *(short*)(buffer + sizeof(int) + i * slogSize() + 38);
 
 			// ako je napon pao ispod 200, ispisati poruku o tome
-            if (Napon[brojVrednosti % maxBrVrednosti] < 200)
+            if (napon[brVrednosti % brPoslednjihVrednosti] < 200)
 			{
+				redniBrGreske++;
                 Serial.print("Pao napon ispod 200, redni broj greske: ");
-                redniBrojGreske++;
-                Serial.println(redniBrojGreske);
+                Serial.println(redniBrGreske);
 
                 Serial.print("Na uredjaju: ");
-                Serial.println(Uredjaj[brojVrednosti % maxBrVrednosti]);
+                Serial.println(uredjaj[brVrednosti % brPoslednjihVrednosti]);
                 Serial.print("Na jacinu napona: ");
-                Serial.println(Napon[brojVrednosti % maxBrVrednosti]);
+                Serial.println(napon[brVrednosti % brPoslednjihVrednosti]);
             }
 
-            brojVrednosti++;
-
 			// nakon svakog merenja, ispisivati prosecnu jacinu struje u poslednjih 10 merenja
-			// nema smisla da racunamo ovo sve dok ne dodjemo do desetog merenja
-            if (brojVrednosti > maxBrVrednosti) 
+            if (brVrednosti > brPoslednjihVrednosti) 
 			{
                 int sumaStruje = 0;
 
-                for (int j = 0; j < maxBrVrednosti; j++)
-                    sumaStruje += Struja[j];
+                for (int j = 0; j < brPoslednjihVrednosti; j++)
+                    sumaStruje += struja[j];
                 
                 Serial.print("Prosecna struja je: ");
-                Serial.println(sumaStruje/maxBrVrednosti);
+                Serial.println(sumaStruje / brPoslednjihVrednosti);
             }
         }
-		
         delete[] buffer;
     }
 }
@@ -82,6 +81,8 @@ void setup()
 
     startStopDataGeneration(START_GENERATION, RANDOM, 0, 20, 0.0, 200);
 
+	brVrednosti = -1;
+	redniBrGreske = 0;
     createTask(brojevi, 1000, TASK_ENABLE,  NULL);
 }
 
