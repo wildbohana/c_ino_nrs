4ec3f5e2a5f8e15fada914b339880cf67be67e3d
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-06-10 11:53:55 +0200

    Update demoZadatak91.cpp

--- v19/Zadatak91/demoZadatak91.cpp
+++ v19/Zadatak91/demoZadatak91.cpp
@@ -3,54 +3,49 @@ Napisati program koji prati stanje pwm signala na pinu 26 i ispisuje njegovu duz
 Za generisanje signala koristiti funkciju pwmRandom() sa parametrima 26 i 2000.
 */
 
-#include<arduinoPlatform.h>
-#include<tasks.h>
-#include<interrupts.h>
-#include<stdio.h>
+#include <arduinoPlatform.h>
+#include <tasks.h>
+#include <interrupts.h>
+#include <stdio.h>
 #include <serial.h>
 #include <pwm.h>
 
 extern serial Serial;
 
-#define myPIN 26
+#define pin26 26
 
-int oldState;
-int periodTime;
-int startTime;
-int elapsedTime;
+int staroStanje;
+int vremePerioda;
+int pocetnoVreme;
+int protekloVreme;
 
 void brojevi(int id, void* tptr)
 {
-    static int first = true;
+    static int prvi = true;
 
-    int newState = digitalRead(myPIN);
+    int novoStanje = digitalRead(pin26);
 
 	// dioda iskljucena
-	if (oldState == 1 && newState == 0) 
+	if (staroStanje == 1 && novoStanje == 0) 
 	{
-		// merimo koliko dugo je bila ukljucena
-        elapsedTime = millis() - startTime;
+        protekloVreme = millis() - pocetnoVreme;
 	} 
 	// dioda ukljucena
-	else if (oldState == 0 && newState == 1) 
+	else if (staroStanje == 0 && novoStanje == 1) 
 	{
         int time = millis();
-		// ukupno trajanje perioda
-        periodTime = time - startTime;
-        startTime = time;
+        vremePerioda = time - pocetnoVreme;
+        pocetnoVreme = time;
 
-		// ispisujemo za sve vrednosti osim za prvu
-        if (!first) 
+        if (!prvi) 
 		{
-           Serial.print(elapsedTime);
+           Serial.print(protekloVreme);
            Serial.print(' ');
-           Serial.println(periodTime);
+           Serial.println(vremePerioda);
         }
-
-        first = false;
+        prvi = false;
 	}
-	
-	oldState = newState;
+	staroStanje = novoStanje;
 }
 
 void setup()
@@ -58,12 +53,12 @@ void setup()
 	Serial.begin(9600);
 
 	// pwmRandom(pin, period)
-	pwmRandom(myPIN, 2000);
+	pwmRandom(pin26, 2000);
 
-	oldState = digitalRead(myPIN);
-	startTime = millis();
-	elapsedTime = 0;
-	periodTime = 0;
+	staroStanje = digitalRead(pin26);
+	pocetnoVreme = millis();
+	protekloVreme = 0;
+	vremePerioda = 0;
 
 	createTask(brojevi, 1, TASK_ENABLE, NULL);
 }
