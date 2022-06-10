44ea818c3f82266b184f2e42c60e3b33e777b6af
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-06-10 11:56:25 +0200

    Update demoZadatak103.cpp

--- v20/Zadatak103/demoZadatak103.cpp
+++ v20/Zadatak103/demoZadatak103.cpp
@@ -3,12 +3,12 @@ Napisati program koji ispisuje vreme u koje je bio ocitan najveci broj.
 Pri svakoj promeni najveceg broja, ispisati vreme na ekran.
 */
 
-#include<arduinoPlatform.h>
-#include<tasks.h>
-#include<interrupts.h>
-#include<stdio.h>
-#include<serial.h>
-#include<data.h>
+#include <arduinoPlatform.h>
+#include <tasks.h>
+#include <interrupts.h>
+#include <stdio.h>
+#include <serial.h>
+#include <data.h>
 
 extern serial Serial;
 
@@ -18,28 +18,27 @@ void broj(int id, void* tptr)
 {
     if (available())
     {
-        char *r = read();
+        char* r = read();
         int br = atoi(r + 21);
 
 		if (br > najveci)
 		{
 			najveci = br;
 			Serial.print("Trenutni najveci broj: ");
-			Serial.print(najveci);
+			Serial.println(najveci);
 
 			char datum[12];
 			strncpy(datum, r, 11);
 			datum[11] = '\0';
-			Serial.print(", Datum: ");
+			Serial.print("Datum: ");
 			Serial.print(datum);
 
 			char vreme[9];
 			vreme[8] = '\0';
-			strncpy(vreme, r+12, 8);
-			Serial.print(", Vreme: ");
+			strncpy(vreme, r + 12, 8);
+			Serial.print(", vreme: ");
 			Serial.println(vreme);
 		}
-
         delete[] r;
     }
 }
@@ -50,7 +49,7 @@ void setup()
 
     startStopDataGeneration(START_GENERATION, RANDOM, 0, 100, 0.1, 500);
 
-    najveci = 0;
+    najveci = -1;
     createTask(broj, 50, TASK_ENABLE, NULL);
 }
 
